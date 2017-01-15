8a0cb63982af9d4f1ffd7943c58498731dc6c96c
Anton <tabachkovav@gmail.com>
2017-01-15 22:37:37 +0300

    remove old directories

--- js/services/resume.js
+++
@@ -1,8 +0,0 @@
-app.factory('resume', ['$http', function($http) { 
-    return $http.get('http://localhost:8080/data/feedbacks.json') 
-            .then(function (response) {
-				var data = response.data;
-
-				return data;
-			});
-}]);
